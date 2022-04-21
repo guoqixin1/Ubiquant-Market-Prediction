70ead9e2f905035867252b3a1746546bd8866ea1
qixiguo@tencent.com <1042813577@qq.com>
2022-04-21 12:06:37 +0800

    version 1.1

--- code/M2/main.py
+++ code/M2/main.py
@@ -1,28 +1,24 @@
 import csv
-
+import tensorflow as tf
 import numpy as np
 from model import *
 from tensorflow.keras.callbacks import *
 from tensorflow.keras.initializers import *
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
-from utils import get_data
+from utils import get_data, My_input, Time_embedding, Input_embedding, evalulate
 
 
 def build_model():
-    inp = Input(shape=(SEQ_LEN, 9))
+    D_MODEL = 300
+    SEQ_LEN = 3773
+    lr = 1e-5
+    inp = My_input(shape=(SEQ_LEN, 3773))
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+    x = Input_embedding(inp[1:])
+    x += Time_embedding(inp[0])
 
-    x = Bidirectional(LSTM(128, return_sequences=True))(inp)
-    x = Bidirectional(LSTM(64, return_sequences=True))(x)
-
-    # for i in range(2):
-    x, self_attn = EncoderLayer(
-        d_model=D_MODEL,
-        d_inner_hid=512,
-        n_head=4,
-        d_k=64,
-        d_v=64,
-        dropout=0.2)(x)
+    x, self_attn = EncoderLayer(d_model=D_MODEL, d_inner_hid=512, n_head=4, d_k=64, d_v=64, dropout=0.2)(x)
 
     avg_pool = GlobalAveragePooling1D()(x)
     max_pool = GlobalMaxPooling1D()(x)
@@ -39,19 +35,21 @@ def build_model():
 
 
 if __name__ == '__main__':
+    EPOCHS = 10
+    BATCH_SIZE = 128
+
     multi_head = build_model()
     # multi_head.summary()
-    X_train, y_train = get_data()
-    callback = EarlyStopping(monitor='val_loss',
-                             patience=3,
-                             restore_best_weights=True)
-    history = multi_head.fit(x=X_train,
-                             y=y_train,
-                             batch_size=BATCH_SIZE,
-                             epochs=EPOCHS,
-                             validation_data=(X_valid, y_valid),
-                             callbacks=[callback])
+    trains, valids, tests = get_data()
+    X_train, Y_train = trains
+    X_valid, Y_valid = valids
+    X_test, Y_test = tests
+    callback = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+    history = multi_head.fit(x=X_train, y=Y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
+                             validation_data=(X_valid, Y_valid), callbacks=[callback])
+    predicted_stock_price_multi_head = multi_head.predict(X_test)
     predicted_stock_price = np.vstack((np.full((60, 1), np.nan), predicted_stock_price_multi_head))
+    evalulate(predicted_stock_price, Y_test)
     with open('../../data/final_res.csv', 'w')as f:
         csv_writer = csv.writer(f)
         csv_writer.writerows(predicted_stock_price)
